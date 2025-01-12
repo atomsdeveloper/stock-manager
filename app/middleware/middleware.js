987bfaf90d53eb7772b4f364651b4a867e2695a9
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-12 20:20:00 -0300

    fix: change checking of api key in header request

--- app/middleware/middleware.js
+++ app/middleware/middleware.js
@@ -3,14 +3,9 @@ require('dotenv').config();
 const hasKey = (req, res, next) => {
     const key = req.headers['x-api-key'];
 
-    if(!key) {
-        return res.status(401).json({message: 'key is missing.'})
-    };
-
-    if(key != process.env.API_KEY_INSERT_USER) {
+    if(!key || key != process.env.API_KEY_INSERT_USER) {
         return res.status(403).json({message: 'invalid key'});
     }
-
     next();
 };
 
