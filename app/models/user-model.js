cbc73a25f51731cbafd249f3d8d6647c85653b7b
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-12 20:18:54 -0300

    feat: created criptografy to storage and fetch pass

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -1,31 +1,56 @@
+// Hash
+const bycrypt = require('bcrypt');
+
+// Prisma
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const hasUser = async (user, pass) => {
+const hasUser = async (user, email, pass) => {
     try {
-        if(user && pass) {
-            return {user, pass};
-        } else {
-            return false
-        }
+        // check session user
+
+        const userExist = await prisma.user.findUnique({
+            where: {
+                name: user,
+                email: email,
+                password: pass,
+            },
+        });
+
+        const match = await bycrypt.compare(pass, userExist.password);
+
+        if(userExist && match) {
+            return {userExist};
+        };
+        throw new Error('not is possible return a user.');
     } catch (error) {
         throw error;
     };
 };
 
-const insertUser = async (name, email, pass) => {
+const insertUserManager = async (name, email, pass) => {
+    const saltRounds = process.env.SALT_ROUNDS;
+    const hashPass = await bycrypt.hash(pass, saltRounds);
+
     try {
-        if(name && email && pass) {
-            return {name, email, pass};
-        } else {
-            return false
-        }
+        const user = await prisma.userManager.create({
+            data: {
+                name: name,
+                email: email,
+                password: hashPass,
+                created_at: new Date()
+            },
+        })
+
+        if(user) {
+            return user;
+        };
     } catch (error) {
-        throw error;
+        throw res.status(500).json({message: "internal database error."});
     };
 };
 
 module.exports = {
     hasUser,
-    insertUser
+    insertUserManager
 };
