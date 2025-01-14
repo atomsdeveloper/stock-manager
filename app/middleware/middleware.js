b26b722edf2d037039849b932b6b040c9c0d1559
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-13 21:06:46 -0300

    feat: created new function middleware checkFields

--- app/middleware/middleware.js
+++ app/middleware/middleware.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const hasKey = (req, res, next) => {
-    const key = req.headers['x-api-key'];
+const hasKey = async (req, res, next) => {
+    const key = await req.headers['x-api-key'];
 
     if(!key || key != process.env.API_KEY_INSERT_USER) {
         return res.status(403).json({message: 'invalid key'});
@@ -9,6 +9,16 @@ const hasKey = (req, res, next) => {
     next();
 };
 
+const checkFields = async (req, res, next) => {
+    const {user, email, pass} = req.body
+
+    if(!user || !email || !pass) {
+        return res.status(422).json({message: 'not content'});
+    }
+    next();
+}
+
 module.exports = {
     hasKey,
+    checkFields,
 }
