ee539a21a7a7a6e8d1b541c140450703a43e704a
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-13 21:09:24 -0300

    fix: refactor code functions controllers

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -9,12 +9,11 @@ const home = (req, res) => res.status(200).render('home');
 const login = async (req, res) => {
     try {
         const {user, email, pass} = req.body;
+        console.log(user)
+        
         const hasUser = await model.hasUser(user, email, pass);
+
         if (hasUser) {
-            const {user, email, pass} = hasUser;
-            if (user && email && pass == undefined || null) {
-                res.status(201).json({message: "not content."});
-            }
             return res.redirect('/home');
         } else {
             res.status(401).json({message: "invalid credentials."});
@@ -27,15 +26,17 @@ const login = async (req, res) => {
 const registerUserManager = async  (req, res) => {
     try {
         const {name, email, password} = req.body;
-        const register = await model.insertUserManager(name, email, password);
-        console.log(register)
-        if(register) {
-            return res.status(200).json({message: 'insert user sucessful.'});
-        } else {
-            res.status(401).json({message: "faleid insert user."});
+        const response = await model.insertUserManager(name, email, password);
+
+        if (response == null) {
+            return res.status(422).json({message: 'insert user failed per duplicate email.'});
         }
-    } catch {
-        return res.status(500).json({message: "internal server error."});
+
+        if(response.email) {
+            return res.status(200).json({message: 'insert user sucessful.'});
+        }    
+    } catch (error) {
+        return res.status(500).json({message: "internal server error.", error});
     }
 };
 
