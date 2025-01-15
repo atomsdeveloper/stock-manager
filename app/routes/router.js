ad974f9c7b975c7b634bbe1ccb64f61a5608f24b
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-15 19:23:34 -0300

    fix: remove routes useless

--- app/routes/router.js
+++ app/routes/router.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const controller = require('../controller/controller.js');
 const middleware = require('../middleware/middleware.js');
 
-// pages
-router.get('/', controller.enter);
-router.get('/home', controller.home)
-
 // routes
 router.post('/login', middleware.checkFields, controller.login);
 router.post('/register',  middleware.checkFields, middleware.hasKey, controller.registerUserManager, ); // register user stock private
