7066addfa7c6d891d2ab9a6d554c9f520d5370f6
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-17 20:21:24 -0300

    feat: change form to other component and add routes with react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,86 +1,54 @@
-import React, {useState} from "react";
-import Swal from 'sweetalert';
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+// Components
+import Index from "./components/index";
+import Home from "./components/home";
 
 // Styles
-import './assets/css/index.css';
 import './assets/css/app.css';
 
-const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const request = async (event) => {
-    event.preventDefault();
-
-    if (!name || !email || !password) {
-      Swal.fire({
-          icon: 'error',
-          title: 'Ops...',
-          text: 'All fields are required',
-          footer: '<a href="#">Why do I have this issue?</a>'
-      });
-      return;
-    }
-
-    try {
-      const response = await fetch('http://localhost:8080/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name, email, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-        console.error(`Error ${response.status}: ${response.statusText}`);
-      }
-
-      await response.json();
-      window.location.href = 'http://localhost:8080/home';
-
-    } catch (error) {
-      console.error('Error:', error);
-    }
-}
+export const Error = () => {
   return (
-    <div className="container">
-      <form className="form-entrar" onSubmit={request}>
-        <div className="form-group">
-          <input
-              type="text"
-              name="usuario"
-              placeholder="Usuário"
-              className="form-control"
-              autoComplete="off"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-          />
-          <input
-              type="email"
-              name="email"
-              placeholder="E-mail"
-              className="form-control"
-              autoComplete="off"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-              type="password"
-              name="senha"
-              placeholder="Senha"
-              className="form-control"
-              autoComplete="off"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit" className="btn btn-success">
-             Entrar
-          </button>
-        </div>
-      </form>
+    <div class="container">
+      <h1>Error</h1>
     </div>
+  );
+}
+
+export const PrivateRoute = ({children}) => {
+  const token = localStorage.getItem('token');
+
+  if(!token) {
+    return (
+      <Navigate to={"/"}/>
+    )
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+          <Route path="/home" element={
+            <>
+              <PrivateRoute>
+                <Home/>
+              </PrivateRoute>
+            </>
+          } 
+          />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   )
 }
 
