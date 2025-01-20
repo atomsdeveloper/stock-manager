17c341ad5636fa7f96dd2f8e95626c2e081a8a20
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-20 20:39:58 -0300

    feat: add new componentes in template login

--- frontend/src/components/index/index.jsx
+++ frontend/src/components/index/index.jsx
@@ -13,43 +13,43 @@ const Index = () => {
     const [password, setPassword] = useState("");
 
     const request = async (event) => {
-    event.preventDefault();
+      event.preventDefault();
 
-    if (!name && !email && !password) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Ops...',
-        text: 'All fields are required',
-        footer: '<a href="#">Why do I have this issue?</a>'
-      });
-    }
-
-    try {
-      const response = await fetch('http://localhost:8080/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name, email, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response failed');
+      if (!name && !email && !password) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ops...',
+          text: 'All fields are required',
+          footer: '<a href="#">Why do I have this issue?</a>'
+        });
       }
 
-      const data = await response.json();
-      if (!data) {
-        throw new Error('Data response not exists.');
-      } 
+      try {
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password }),
+        });
 
-      localStorage.setItem('name', data.user.name);
-      localStorage.setItem('email', data.user.email);
-      localStorage.setItem('token', data.token);
+        if (!response.ok) {
+          throw new Error('Network response failed');
+        }
 
-     navigate('/home')
-    } catch (error) {
-      console.error('Error:', error);
-    }
+        const data = await response.json();
+        if (!data) {
+          throw new Error('Data response not exists.');
+        } 
+
+        localStorage.setItem('name', data.user.name);
+        localStorage.setItem('email', data.user.email);
+        localStorage.setItem('token', data.token);
+
+        navigate('/home')
+      } catch (error) {
+        console.error('Error:', error);
+      }
 }
   return (
     <div className="container">
@@ -57,10 +57,14 @@ const Index = () => {
 
         <form className="form-entrar" onSubmit={request}>
           <div className="form-group">
+            <div className="form-title">
+              <h1>Bem vindo ao <br /> Gerenciador de Estoque.</h1>
+              <img src="/icons/icon-box.png" alt="Icone do Título do Formulário." />
+            </div>
             <input
               type="text"
               name="usuario"
-              placeholder="Usuário"
+              placeholder="Dígite seu usuário..."
               className="form-control"
               autoComplete="off"
               value={name}
@@ -69,7 +73,7 @@ const Index = () => {
             <input
               type="email"
               name="email"
-              placeholder="E-mail"
+              placeholder="Dígite seu e-mail..."
               className="form-control"
               autoComplete="off"
               value={email}
@@ -78,7 +82,7 @@ const Index = () => {
             <input
               type="password"
               name="senha"
-              placeholder="Senha"
+              placeholder="Dígite seu senha..."
               className="form-control"
               autoComplete="off"
               value={password}
