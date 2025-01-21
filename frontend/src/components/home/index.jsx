e8712e9e6068aa312ec4f575f982debe4e9138ee
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-21 19:10:30 -0300

    fix: page navigate after fetch api

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,38 +1,46 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Styles
 import '../../assets/css/index.css';
 
-const Home = async () => {
-  const navigate = useNavigate();
-  const token = localStorage.getItem('token');
+const Home = () => {
+  const navigate = useNavigate(); 
 
-  if (!token) {
-    // Redireciona para login se o token não existir
-    navigate('/');
-    return;
-  }
-
-  try {
-    const response = await fetch('http://localhost:8080/home', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Unauthorized access');
+  useEffect(() => {
+    const requestToken = async () => {
+      const token = localStorage.getItem('token');
+  
+      if (!token) {
+        // Redireciona para login se o token não existir
+        navigate('/');
+        return;
+      }
+    
+      try {
+        const response = await fetch('http://localhost:8080/home', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+    
+        if (!response.ok) {
+          navigate('/')
+          throw new Error('Unauthorized access');
+        }
+    
+      } catch (error) {
+        navigate('/'); 
+      }
     }
 
-  } catch (error) {
-    navigate('/'); 
-  }
+    requestToken();
+  }, [navigate])
 
   return (
-    <div className="">
+    <div>
        <h1>Home</h1>
     </div>
   )
